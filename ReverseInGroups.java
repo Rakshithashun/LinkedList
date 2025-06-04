ee334b9e863166d9e3227de1b4169214dafd3db1
Rakshitha <118209233+Rakshithashun@users.noreply.github.com>
2025-06-04 12:30:50 +0530

    ReverseInGroups

---
+++ ReverseInGroups.java
@@ -0,0 +1,107 @@
+class LinkedList{
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    Node head=null;
+    public void insertAtBeg(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=null;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    public void insertAtEnd(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            newNode.next=null;
+            return;
+        }
+        Node current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=newNode;
+    }
+    public void insertAtPos(int data,int pos){
+        Node newNode=new Node(data);
+        Node current=head;
+        while(current!=null && pos!=1){
+            current=current.next;
+            pos--;
+            }
+            
+        newNode.next=current.next;
+        current.next=newNode;
+        
+    }
+    public void deleteAtBeg(){
+        if(head==null){
+            System.out.print("Empty");
+            return;
+        }
+        head=head.next;
+    }
+    public void deleteAtEnd(){
+        if(head==null){
+            System.out.print("Empty");
+            return;
+        }
+        Node current=head;
+        while(current.next.next!=null){
+            current=current.next;
+        }
+        current.next=null;
+    }
+    public void display(){
+        Node current=head;
+        while(current!=null){
+            System.out.print(current.data+"->");
+            current=current.next;
+        }
+        System.out.println("null");
+    }
+    
+    public Node revGroups(Node head,int n){
+        Node current=head,prev=null,next=null;
+        int count=0;
+        while(current!=null && count<n){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+            count++;
+        }
+        if(next!=null){
+            head.next=revGroups(next,n);
+            
+        }
+        return prev;
+        
+    }
+    public void ReverseInGroups(int n){
+        head=revGroups(head,n);
+    }
+}
+
+public class Main{
+     public static void main(String[] args){
+         LinkedList l=new LinkedList();
+         l.insertAtBeg(5);
+         l.insertAtBeg(4);
+         l.insertAtEnd(6);
+         l.insertAtEnd(7);
+         l.insertAtEnd(8);
+         l.insertAtEnd(9);
+         l.ReverseInGroups(3);
+         l.display();
+    }
+    
+}
